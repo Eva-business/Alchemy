2a44d689c12a5810c43eac6307ebc0a9d7b674c7
GaryHu <garyhu17@gmail.com>
2025-11-19 21:03:47 +0800

    Initial Commit

---
+++ Alchemy/AlchemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlchemyApp.swift
+//  Alchemy
+//
+//  Created by user13 on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct AlchemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alchemy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alchemy
+//
+//  Created by user13 on 2025/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
